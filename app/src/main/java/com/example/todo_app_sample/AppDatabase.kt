f99a6dce2c626ce153a2a427a335ab5949d2c056
Tao <momossy5@gmail.com>
2020-07-10 15:52:19 +0900

    add AppDatabase.kt

--- app/src/main/java/com/example/todo_app_sample/AppDatabase.kt
+++ app/src/main/java/com/example/todo_app_sample/AppDatabase.kt
@@ -1,5 +1,31 @@
 package com.example.todo_app_sample
 
-abstract class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    abstract val todoDao: TodoDao
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "app_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
